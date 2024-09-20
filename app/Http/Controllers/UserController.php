bd27b4b33ac949d627a1a7d36047f28468e41231
fitrianur2411 <fn394585@gmail.com>
2024-09-20 08:10:23 +0700

    JS4 - Praktikum 1 - $fillable:

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,13 +18,33 @@ class UserController extends Controller
             'password' => Hash::make('12345'), 
             'level_id' => 4 
         ]; 
-        UserModel::insert($data); // tambahkan data ke tabel m_user */
+        UserModel::insert($data); // tambahkan data ke tabel m_user 
 
         $data = [
             'nama'=> 'Pelanggan Pertama',
         ];
-        userModel::where('username', 'customer-1')->update($data); //update data user
- 
+        userModel::where('username', 'customer-1')->update($data);*/ //update data user
+        
+        //JS4 Praktikum 1 
+	    //Langkah 2
+        /*$data = [ 
+            'level_id' => 2, 
+            'username' => 'manager_dua', 
+            'nama' => 'Manager 2', 
+            'password' => Hash::make('12345') 
+        ]; 	
+	     UserModel::create($data);*/
+
+        //Langkah 5
+        $data = [ 
+            'level_id' => 2, 
+            'username' => 'manager_tiga', 
+            'nama' => 'Manager 3', 
+            'password' => Hash::make('12345') 
+        ]; 
+            UserModel::create($data) ; 
+
+
         // coba akses model UserModel 
         $user = UserModel::all(); // ambil semua data dari tabel m_user 
         return view('user', ['data' => $user]); 
