02aa429e94a72211cf58a92b9a6027b5877a4a22
fitrianur2411 <fn394585@gmail.com>
2024-10-18 10:09:17 +0700

    JS8 - Tugas 1-3_Level

--- resources/views/level/index.blade.php
+++ resources/views/level/index.blade.php
@@ -1,37 +1,39 @@
 @extends('layouts.template')
 @section('content')
-    <div class="card card-outline card-primary">
+    <div class="card">
         <div class="card-header">
-            <h3 class="card-title">{{ $page->title }}</h3>
+            <h3 class="card-title">Daftar level</h3>
             <div class="card-tools">
-                <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('level/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+                <button onclick="modalAction('{{ url('/level/import') }}')" class="btn btn-info">Import level</button>
+                <a href="{{ url('/level/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export level</a>
+                <a href="{{ url('/level/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export level</a>
+                <button onclick="modalAction('{{ url('/level/create_ajax') }}')" class="btn btn-success">Tambah Data
+                    (Ajax)</button>
             </div>
         </div>
         <div class="card-body">
-            @if (@session('success'))
-                <div class="alert alert-success">{{ session('success')}}</div>
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
             @endif
             @if (session('error'))
-                <div class="alert alert-danger">{{session('error')}}</div>
+                <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+            <table class="table table-bordered table-sm table-striped table-hover" id="table-level">
                 <thead>
                     <tr>
-                        <th >ID</th>
-                        <th>Kode</th>
-                        <th >Nama</th>
-                        <th >Aksi</th>
+                        <th>No</th>
+                        <th>Kode level</th>
+                        <th>Nama level</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
+                <tbody></tbody>
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" 
-    data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false"
+        data-width="75%"></div>
 @endsection
-@push('css')
-@endpush
 @push('js')
     <script>
         function modalAction(url = '') {
@@ -39,45 +41,55 @@
                 $('#myModal').modal('show');
             });
         }
-
-        var dataLevel;
-		$(document).ready(function() {
-			 dataLevel = $('#table_level').DataTable({
-				// serverSide: true, jika ingin menggunakan server side processing
-				serverSide: true,
-				ajax: {
-					"url": "{{ url('level/list') }}",
-					"dataType": "json",
-					"type": "POST"
-				},
+        var tableLevel;
+        $(document).ready(function() {
+            tableLevel = $('#table-level').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    "url": "{{ url('level/list') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                    "data": function(d) {
+                        d.filter_kategori = $('.filter_kategori').val();
+                    }
+                },
                 columns: [{
-                    // nomor urut dari laravel datatable addIndexColumn()
-                    data: "DT_RowIndex",
+                     // nomor urut dari laravel datatable addIndexColumn()
+                     data: "DT_RowIndex",
                     className: "text-center",
+                    width: "10%",
                     orderable: false,
                     searchable: false
                 }, {
+
                     data: "level_kode",
                     className: "",
-                    // orderable: true, jika ingin kolom ini bisa diurutkan
+                    width: "10%",
                     orderable: true,
-                    // searchable: true, jika ingin kolom ini bisa dicari
                     searchable: true
                 }, {
                     data: "level_nama",
                     className: "",
+                    width: "37%",
                     orderable: true,
-                    searchable: true
+                    searchable: true,
                 }, {
                     data: "aksi",
-                    className: "",
+                    className: "text-center",
+                    width: "14%",
                     orderable: false,
                     searchable: false
                 }]
             });
-            $('#level_id').on('change',function(){
-                dataLevel.ajax.reload();
-            })
+            $('#table-level_filter input').unbind().bind().on('keyup', function(e) {
+                if (e.keyCode == 13) { // enter key
+                    tableLevel.search(this.value).draw();
+                }
+            });
+            $('.filter_kategori').change(function() {
+                tableLevel.draw();
+            });
         });
     </script>
 @endpush
