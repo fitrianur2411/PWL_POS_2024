fda7484f46b14853ee5e977a777c9bb0ccb9c772
fitrianur2411 <fn394585@gmail.com>
2024-11-01 08:13:00 +0700

    JS10 - Praktikum 1 – Membuat RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,20 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 // class UserModel extends Model
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //Mendefiniskan primary key dari tabel yang digunakan
     protected $fillable = ['username','password','nama','level_id','avatar','created_at', 'updated_at'];
     
-    protected $hidden = ['password']; // jangan di tampilkan saat select
+    // protected $hidden = ['password']; // jangan di tampilkan saat select
     
     protected $casts=  ['password'=> 'hashed']; // casting password agar otomatis di hash
-
+    
     // protected $fillable = ['level_id', 'username', 'nama'];
     public function level(): BelongsTo 
     { 
